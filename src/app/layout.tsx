84cd06a605a2ccbd36751b5e49949b60015fb54e
Briofita09 <felipe.osorio.santos@hotmail.com>
2024-05-13 00:16:52 -0300

    feat: icon to metadata

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -8,6 +8,7 @@ const inter = Inter({ subsets: ["latin"] });
 export const metadata: Metadata = {
   title: "Convite Aurora",
   description: "Venha para o meu sexto aniversário",
+  icons: 'https://www.canva.com/design/DAGCtosflbE/-YKb_MNFzru4B5MabWWjcA/view'
 };
 
 export default function RootLayout({

7d2cca797569b05f7f6d086b89db917cd888b8f4
Briofita09 <felipe.osorio.santos@hotmail.com>
2024-05-13 00:09:11 -0300

    first commit

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,113 +1,157 @@
+'use client'
+
+import { useState } from "react";
+
 import Image from "next/image";
+import { Button, HStack, Input, List, ListIcon, ListItem, useNumberInput } from "@chakra-ui/react";
+import { Hearts } from 'react-loader-spinner'
+
+import everest from '@/logos/everest.png'
+import skye from '@/logos/patrulha-canina-skye-11.png'
+import pawLogo from '@/logos/paw_logo.svg'
+
+
 
 export default function Home() {
+  const [name, setName] = useState<string>('')
+  const [adultsValue, setAdultsValue] = useState<number>(0);
+  const [kidsValue, setKidsValue] = useState<number>(0);
+  const [loading, setLoading] = useState<boolean>(false)
+  const [hints, setHints] = useState<boolean>(false)
+  const [thanks, setThanks] = useState<boolean>(true)
+  const {
+    getInputProps: getAdultsInputProps,
+    getIncrementButtonProps: getAdultsIncrementButtonProps,
+    getDecrementButtonProps: getAdultsDecrementButtonProps,
+  } = useNumberInput({
+    step: 1,
+    defaultValue: 0,
+    min: 0,
+    max: 6,
+    onChange: (valueString) => setAdultsValue(parseInt(valueString)),
+    isDisabled: loading
+  });
+
+  const adults = getAdultsInputProps();
+
+  const {
+    getInputProps: getKidsInputProps,
+    getIncrementButtonProps: getKidsIncrementButtonProps,
+    getDecrementButtonProps: getKidsDecrementButtonProps,
+  } = useNumberInput({
+    step: 1,
+    defaultValue: 0,
+    min: 0,
+    max: 6,
+    onChange: (valueString) => setKidsValue(parseInt(valueString)),
+    isDisabled: loading
+  });
+  const kids = getKidsInputProps();
+
+  async function handleSubmit() {
+    setLoading(true)
+  }
+
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <div className="z-10 w-full max-w-5xl items-center justify-between font-mono text-sm lg:flex">
-        <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
-          Get started by editing&nbsp;
-          <code className="font-mono font-bold">src/app/page.tsx</code>
-        </p>
-        <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:size-auto lg:bg-none">
-          <a
-            className="pointer-events-none flex place-items-center gap-2 p-8 lg:pointer-events-auto lg:p-0"
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{" "}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className="dark:invert"
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
+    <main className="bg-pink-300 min-h-screen p-4">
+      <div className="flex justify-around">
+        <div className="flex flex-col justify-center">
+          <Image src={everest} alt="everest" height={80} />
         </div>
+        <div>
+          <h1 className="font-bold text-center text-2xl">Aurora</h1>
+          <h2 className="text-center">convida vocês para o seu</h2>
+          <h2 className="text-center">sexto aniversário</h2>
+
+          <h2 className="text-xl text-center font-bold mt-4">Quando?</h2>
+          <p className="text-center">Sábado, 8 de Junho</p>
+
+          <h2 className="text-xl text-center font-bold mt-4">Onde?</h2>
+          <p className="text-center">Rua, Fabio Montenegro, 10</p>
+
+        </div>
+        <div className="flex flex-col justify-center">
+          <Image src={skye} alt="skye" height={80} />
+        </div>
+
       </div>
 
-      <div className="relative z-[-1] flex place-items-center before:absolute before:h-[300px] before:w-full before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-full after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700 before:dark:opacity-10 after:dark:from-sky-900 after:dark:via-[#0141ff] after:dark:opacity-40 sm:before:w-[480px] sm:after:w-[240px] before:lg:h-[360px]">
-        <Image
-          className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert"
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
+
+      <div className="w-[90vw] flex items-center mt-2">
+        <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3645.893233721446!2d-46.33168368859918!3d-23.964215276427336!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x94ce030f539a7c39%3A0x8f1bdaf5af42e6da!2sR.%20F%C3%A1bio%20Montenegro%2C%2010%20-%20Gonzaga%2C%20Santos%20-%20SP%2C%2011060-475!5e0!3m2!1spt-BR!2sbr!4v1715562587210!5m2!1spt-BR!2sbr" width="100%" height="180" style={{ border: '0px' }} loading="lazy" referrerPolicy="no-referrer-when-downgrade"></iframe>
       </div>
+      {
+        !hints ? (
+          <form className="mt-4 flex flex-col gap-3">
 
-      <div className="mb-32 grid text-center lg:mb-0 lg:w-full lg:max-w-5xl lg:grid-cols-4 lg:text-left">
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className="mb-3 text-2xl font-semibold">
-            Docs{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className="m-0 max-w-[30ch] text-sm opacity-50">
-            Find in-depth information about Next.js features and API.
-          </p>
-        </a>
+            <div className="flex justify-between items-center">
+              <label htmlFor="name">Seu nome:</label>
+              <input type="text" className="bg-yellow-200 rounded-md w-60 text-black p-2" onChange={(e) => setName(e.target.value)} disabled={loading} />
+            </div>
+            <div className="flex justify-between items-center">
+              <label>Número de adultos</label>
 
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className="mb-3 text-2xl font-semibold">
-            Learn{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className="m-0 max-w-[30ch] text-sm opacity-50">
-            Learn about Next.js in an interactive course with&nbsp;quizzes!
-          </p>
-        </a>
+              <HStack maxW='140px'>
 
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className="mb-3 text-2xl font-semibold">
-            Templates{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className="m-0 max-w-[30ch] text-sm opacity-50">
-            Explore starter templates for Next.js.
-          </p>
-        </a>
+                <Button {...getAdultsDecrementButtonProps()}>-</Button>
+                <Input {...adults} bgColor='#fff894' variant='filled' />
+                <Button {...getAdultsIncrementButtonProps()}>+</Button>
+              </HStack>
+            </div>
 
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className="mb-3 text-2xl font-semibold">
-            Deploy{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className="m-0 max-w-[30ch] text-balance text-sm opacity-50">
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
-      </div>
-    </main>
+            <div className="flex justify-between items-center">
+              <label>Número de crianças</label>
+
+              <HStack maxW='140px'>
+
+                <Button {...getKidsDecrementButtonProps()}>-</Button>
+                <Input {...kids} bgColor='#fff894' variant='filled' />
+                <Button {...getKidsIncrementButtonProps()}>+</Button>
+              </HStack>
+            </div>
+
+
+            <Button bgColor='#fff894' onClick={handleSubmit} isDisabled={loading}>
+              {!loading ? "Confirmar Presença" : <Hearts
+                height="80"
+                width="80"
+                color="#d99ec3"
+                ariaLabel="hearts-loading"
+                wrapperStyle={{}}
+                wrapperClass=""
+                visible={true}
+              />}
+            </Button>
+
+          </form>
+        ) :
+          (
+            <List spacing={3} mt={4}>
+              <ListItem className="flex items-center">
+                <ListIcon as={Image} src={pawLogo} boxSize={8} />
+                Aurora calça 27 e veste 6-8
+              </ListItem>
+              <ListItem className="flex items-center">
+                <ListIcon as={Image} src={pawLogo} boxSize={8} />
+                Ama brilho e cores
+              </ListItem>
+              <ListItem className="flex items-center">
+                <ListIcon as={Image} src={pawLogo} boxSize={8} />
+                Adora roupas, laços e calçados
+              </ListItem>
+              <ListItem className="flex items-center">
+                <ListIcon as={Image} src={pawLogo} boxSize={8} />
+                Gosta de jogos de tabuleiro e itens de maquiagem/beleza
+              </ListItem>
+              <ListItem className="flex items-center">
+                <ListIcon as={Image} src={pawLogo} boxSize={8} />
+                Aprecia bichos de pelucia
+              </ListItem>
+            </List>
+          )
+      }
+
+      <h3 className="mt-5 text-center font-bold" onClick={() => { setHints(!hints); setThanks(false) }}>{!hints ? "Ideias de presente? Clique aqui!" : "Confirme sua presença clicando aqui!"}</h3>
+    </main >
   );
 }

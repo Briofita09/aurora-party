f514f20de7a079cf677da8c4474ac528e3c28485
Briofita09 <felipe.osorio.santos@hotmail.com>
2024-08-19 01:14:58 -0300

    feat: adam party

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -9,9 +9,14 @@ const config: Config = {
   theme: {
     extend: {
       backgroundImage: {
+        "custom-image":
+          "url('https://e1.pxfuel.com/desktop-wallpaper/373/387/desktop-wallpaper-homepage-hot-wheels-unleashed.jpg')",
         "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
         "gradient-conic":
           "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
+      },
+      textColor: {
+        DEFAULT: "#FFFFFF",
       },
     },
   },
